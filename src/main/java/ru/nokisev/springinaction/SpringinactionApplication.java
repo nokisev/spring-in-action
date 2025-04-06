2801a838ab6cadf0afe6a25ab129a138eb6a62e2
nokisev <nokisev@mail.ru>
2025-04-06 14:44:16 +0300

    1.2. Разработка веб-приложений.

--- src/main/java/ru/nokisev/springinaction/SpringinactionApplication.java
+++ src/main/java/ru/nokisev/springinaction/SpringinactionApplication.java
@@ -2,12 +2,18 @@ package ru.nokisev.springinaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringinactionApplication {
+public class SpringinactionApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringinactionApplication.class, args);
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 }
